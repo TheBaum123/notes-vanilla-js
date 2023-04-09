672e61419e45ce867215151bd8f4a0dc9da5ead1
The_Baum <102868970+TheBaum123@users.noreply.github.com>
2023-04-09 22:04:28 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const textbox = document.getElementById("textbox")
+
+if(localStorage.getItem("notePage:text")!= null) {
+    console.log(localStorage.getItem("notePage:text"))
+    textbox.innerHTML = localStorage.getItem("notePage:text")
+}
+
+document.addEventListener("keydown", (e) => {
+    textbox.removeChild(document.getElementById("cursor"))
+    if(e.ctrlKey) {
+        if(e.key == "n") {
+            textbox.innerHTML = ""
+        }
+    } else if(e.key == " ") {
+        const inputtoadd = document.createTextNode(" ")
+        textbox.appendChild(inputtoadd)
+    } else if(e.key.length == 1) {
+        const spanthing = document.createElement("span")
+        const inputtoadd = document.createTextNode(e.key)
+        spanthing.appendChild(inputtoadd)
+        textbox.appendChild(spanthing)
+    } else if(e.key == "Enter") {
+        const inputtoadd = document.createElement("br")
+        textbox.append(inputtoadd)
+    } else if(e.key == "Backspace") {
+        if(textbox.childNodes.length >= 1)
+        textbox.removeChild(textbox.lastChild)
+    }
+    const cursor = document.createElement("div")
+    cursor.id = "cursor"
+    textbox.appendChild(cursor)
+    localStorage.setItem("notePage:text", textbox.innerHTML)
+})
